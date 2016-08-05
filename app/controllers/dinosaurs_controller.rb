bc43866c9669062811cc301d3224df84e0c2ab22
Julian van der Giesen <julianvandergiesen@hotmail.com>
2016-08-05 13:56:34 +0200

    Create individual dinosaur pages

---
+++ app/controllers/dinosaurs_controller.rb
@@ -0,0 +1,11 @@
+class DinosaursController < ApplicationController
+    
+    def index
+        @dinosaurs = Dinosaur.all
+    end
+    
+    def show
+        @dinosaur = Dinosaur.find(params[:id])
+    end
+    
+end
